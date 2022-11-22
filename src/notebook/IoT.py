c1deaec905959a030cdffce04c99722ea50b5af6
marzie.rasooli@gmail.com <marzie.rasooli@gmail.com>
2022-11-22 21:02:41 +0000

    update telematics data location

--- src/notebook/IoT.py
+++ src/notebook/IoT.py
@@ -10,13 +10,9 @@ json_['database_schema_name']
 # MAGIC %sql
 # MAGIC create table if not exists smart_claims_new.telematics
 # MAGIC using delta location
-# MAGIC "dbfs:/user/hive/warehouse/smart_claims.db/telematic";
+# MAGIC "../../resources/data/telematic";
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC Select * from smart_claims_new.telematics
-
-# COMMAND ----------
-
-
